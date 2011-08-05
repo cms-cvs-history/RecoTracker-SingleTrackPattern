5f0009cb34f80a58eea5549b1788c9217e92105a
William Tanenbaum <sha1-faefd172b4369b2002e8f0f7d11f304fd9896b5b@cern.ch>
2011-08-05 18:45:03 +0000

    Remove #include of obsolete empty header

--- test/SealModules.cc
+++ test/SealModules.cc
@@ -1,4 +1,3 @@
-#include "FWCore/PluginManager/interface/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
 #include "RecoTracker/SingleTrackPattern/test/ReadCosmicTracks.h"
 #include "RecoTracker/SingleTrackPattern/test/AnalyzeMTCCTracks.h"
